8f678fdfe60d112dad900e7810bd990bba82b8a4
conshipl <conshipl@tesla.cs.iupui.edu>
2023-11-29 14:34:27 -0500

    Create Expr_Node Class for Composite/Visitor Pattern

---
+++ Expr_Node.cpp
@@ -0,0 +1,11 @@
+#include "Expr_Node.h"
+
+Expr_Node::Expr_Node (void)
+{
+
+}
+
+Expr_Node::~Expr_Node (void)
+{
+
+}
---
+++ Expr_Node.h
@@ -0,0 +1,15 @@
+#ifndef _EXPR_NODE_H_
+#define _EXPR_NODE_H_
+
+#include "Expr_Node_Visitor"
+
+class Expr_Node 
+{
+public:
+  Expr_Node (void);
+  virtual ~Expr_Node (void);
+
+  virtual void accept (Expr_Node_Visitor & v) = 0;
+};
+
+#endif
