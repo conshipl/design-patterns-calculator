5e550ab27e5f0ae9cdd59e21056ba1acfd7d328a
conshipl <conshipl@tesla.cs.iupui.edu>
2023-11-29 21:53:50 -0500

    Push Eval_Expr_Tree shell

---
+++ Eval_Expr_Tree.cpp
@@ -0,0 +1,41 @@
+#include "Eval_Expr_Tree.h"
+
+Eval_Expr_Tree::Eval_Expr_Tree (void)
+{
+
+}
+
+Eval_Expr_Tree::~Eval_Expr_Tree (void)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Add_Node (const Add_Node & node)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Divide_Node (const Divide_Node & node)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Modulo_Node (const Modulo_Node & node)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Multiply_Node (const Multiply_Node & node)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Number_Node (const Number_Node & node)
+{
+
+}
+
+void Eval_Expr_Tree::Visit_Subtract_Node (const Subtract_Node & node)
+{
+
+}
